4f900d09641901834bef1259f8f0e223f4e87eca
jargr <jaroslav.grablevski@gmail.com>
2023-02-26 15:08:48 +0200

    For loops

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,13 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
-
+    public void printDigitsSum(int t) {
+        int l = (t + "").length();
+        int sum = 0;
+        for (int i = 0; i < l; i++) {
+            sum += t % 10;
+            t = t / 10;
+        }
+        System.out.println(Math.abs(sum));
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int sum = 0;
+        for (int i = 1; i <= lengthOfLastNumber; i++) {
+            int n = 9;
+            for (int j = 2; j <= i; j++) {
+                n = n * 10 + 9;
+            }
+            sum += n;
+        }
+        System.out.println(sum);
     }
 }
